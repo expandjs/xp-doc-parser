0c91bc9ba781cee6a04a669f2492de8872ca7b26
grullo90 <g.rullo@expandjs.com>
2017-12-22 16:05:04 +0100

    Release 1.1.0

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,11 @@ const Uglify = require('uglifyjs-webpack-plugin');
 // Exporting
 module.exports = {
     entry: './index.js',
+    output: {filename: 'xp-doc-parser.js', path: `${__dirname}/dist`},
+    plugins: [new Uglify({uglifyOptions: {output: {comments: /^$/}}})],
     externals: {
         'expandjs': 'XP',
         'xp-emitter': 'XPEmitter',
         'xp-request': 'XPRequest'
-    },
-    output: {
-        filename: 'xp-doc-parser.js',
-        path: `${__dirname}/dist`
-    },
-    plugins: [
-        new Uglify({compress: {warnings: false}, output: {comments: false}})
-    ]
+    }
 };
